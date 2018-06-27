f99ee122741a3be117f4e880c71ff02a1bb071d1
flyburi <yumi.bae@gmail.com>
2018-06-27 21:35:07 +0900

    - Add image paging slider

--- 100days/ViewController.swift
+++ 100days/ViewController.swift
@@ -8,16 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var mainScrollView: UIScrollView!
+    
+    @IBOutlet weak var imagePageControl: UIPageControl!
+    
+    var imageArray = [UIImage]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mainScrollView.frame = view.frame
+        
+        imageArray = [
+            UIImage(named: "cafe1.jpeg")!,
+            UIImage(named: "cafe2.jpeg")!,
+            UIImage(named: "cafe3.jpeg")!
+        ]
+        
+        mainScrollView.delegate = self
+        for i in 0..<imageArray.count{
+            let imageView = UIImageView()
+            imageView.image = imageArray[i]
+            imageView.contentMode = .scaleAspectFit
+            let xPosition = self.view.frame.width * CGFloat(i)
+            imageView.frame = CGRect(x:xPosition, y:0, width:self.mainScrollView.frame.width,
+                                     height:self.mainScrollView.frame.height)
+            mainScrollView.contentSize.width = mainScrollView.frame.width * CGFloat(i + 1)
+            mainScrollView.addSubview(imageView);
+            
+        }
+        
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let page = mainScrollView.contentOffset.x / mainScrollView.frame.size.width
+        imagePageControl.currentPage = Int(page)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
